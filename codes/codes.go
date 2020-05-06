561385d5490a04605be8993ce40c165a0cf3d23c
Daniel Requena <requena@gmail.com>
2020-05-06 00:33:32 -0300

    add CheckHTTPCODE test

--- codes/codes.go
+++ codes/codes.go
@@ -69,9 +69,10 @@ var validCodes = map[int]bool{
 
 // CheckHTTPCODE check if a int is a valid HTTP STATUS CODE
 func CheckHTTPCODE(code int) bool {
+
 	if validCodes[code] {
 		return true
-	} else {
-		return false
 	}
+
+	return false
 }
